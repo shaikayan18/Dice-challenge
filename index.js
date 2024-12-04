9f6f798eb77a0436748c5f5d35c91e745be2ab6c
SHAIK AKHIB K <134771288+shaikayan18@users.noreply.github.com>
2024-12-04 23:06:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let n=prompt("enter your number");
-let arr=[];
-for( let i=1; i<=5; i++){
-    arr[i-1]=i;
+var randomnumber1 = Math.floor(Math.random()*6)+1;
+
+var randomdiceimage = "dice" + randomnumber1 + ".png";
+
+var randomSoourceimage = "images/" + randomdiceimage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomSoourceimage);
+
+var randomnumber2 = Math.floor(Math.random()*6)+1;
+
+var randomdiceimage = "dice" + randomnumber2 + ".png";
+
+var randomSoourceimage = "images/" + randomdiceimage;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomSoourceimage);
+
+if (randomnumber1>randomnumber2){
+	document.querySelector("h1").innerHTML="shaik wins";
 }
-console.log(arr);
+else if(randomnumber1<randomnumber2){
+	document.querySelector("h1").innerHTML="Ayan wins";
+}
+else{
+	document.querySelector("h1").innerHTML="DRAW!";
+}
